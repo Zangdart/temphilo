a7ec243711cdec4750aa4bdaf2498d974996145f
Davy Castanet <davy.castanet@gmail.com>
2021-03-23 21:51:46 -0400

    removed files and added favicon

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,3 @@
-/*
-	Rocket Internet by TEMPLATE STOCK
-    templatestock.co @templatestock
-*/
-
 /* Header Full Screen */
 $(document).ready(function() {
 	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
